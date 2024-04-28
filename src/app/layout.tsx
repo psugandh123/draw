a4c71ba878581f8c8c22ec178b4eb461cf5eaf5c
Sugandhraj Patel <sugandhraj@daolens.com>
2024-04-28 11:41:43 +0530

    space remove

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Geometry Pad",
+  title: "GeometryPad",
   description: "GeometryPad is an interactive canvas app designed for creating and manipulating geometric constructions.",
 };
 
